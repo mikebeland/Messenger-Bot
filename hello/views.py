26ccb4b013cb1c8cb2917caf403374556e63f21b
mikebeland <michael.beland@mymail.champlain.edu>
2018-04-18 15:59:15 -0400

    Update views.py

--- hello/views.py
+++ hello/views.py
@@ -5,7 +5,7 @@ from .models import Greeting
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Hello from Python!')
+    return HttpResponse(200 OK)
     return render(request, 'index.html')
 
 
